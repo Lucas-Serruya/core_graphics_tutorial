3dfa38c5a9971596a0645579f41df5f6d1b6051d
Lucas Serruya <lucas.serruya@etermax.com>
2015-09-18 09:55:45 -0300

    first commit

---
+++ CoolTable/CoolTable/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CoolTable
+//
+//  Created by Lucas Serruya on 9/18/15.
+//  Copyright (c) 2015 Lucas Serruya. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
